a98a8cc767a44e17a903193c1a3fcdd37d543e46
Alan <alanherrera0639@gmail.com>
2023-03-29 02:26:31 -0600

    nuevos cambios

--- cart.php
+++ cart.php
@@ -1,3 +1,6 @@
+<!-- 29/03/2023 Terminación del video 29 -->
+
+
 <!-- conneting files -->
 <?php
     include('includes/connect.php');
@@ -9,7 +12,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Taylor</title>
+    <!-- 29/03/2023 Se cambio el titulo-->
+    <title>Taylor - Detalles del carrito</title>
     <!--bootstrapt CSS link-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!--font Awesome Link-->
@@ -47,21 +51,12 @@
                 <a class="nav-link" href="cart.php"><i class="fa-solid fa-cart-shopping"></i>
                 <sup><?php cart_item();?></sup> </a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Monto total:100/- </a>
-                </li>
+                    <!-- 29/03/2023 Se Elimino un nav-item de Monto Total.-->
                 <li class="nav-item">
                 <a class="nav-link disabled">Disabled</a>
                 </li>
             </ul>
-
-            <!-- search bar -->
-            <form class="d-flex" role="search" action="search_product.php" method="get">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_data">
-                <!-- <button class="btn btn-outline-light" type="submit">Buscar</button> -->
-                <input type="submit" value="Search" class="btn-outline-light" name="search_data_product">
-
-            </form>
+                    <!-- 29/03/2023 Se elimino el From de Search Bar-->
             </div>
         </div>
         </nav>
@@ -87,63 +82,48 @@
             <p class="text-center">De músicos para músicos en todo México</p>
         </div>
 
-        <!--fourth child -->
-        <div class="row px-3">
-            <div class="col-md-10">
-                <!--productos-->
-                <div class="row">
-                    <!-- <div class="col-md-4 mb-2"> -->
-                        <!-- fetching the products -->
-                        <?php 
-                            getProducts();
-                            get_unique_categories();
-                            get_unique_brands();
-                            // $ip = getIPAddress();  
-                            // echo 'User Real IP Address - '.$ip;
-                        ?>
-                    <!-- </div> -->
+       <!-- 29/03/2023  Se borro el Fourth child y se crea Fourth child-table -->
+
+       <!-- Fourth child-table -->
+        <div class="container">
+            <div class="row">
+                <table class="table table-bordered text-center">
+                    <thead>
+                        <tr>
+                            <th>Titulo del Producto</th>
+                            <th>Imagen del Producto</th>
+                            <th>Calidad</th>
+                            <th>Precio Total</th>
+                            <th>Borrar</th>
+                            <th>Operaciones</th>
+                        </tr>
+                        <tbody>
+                            <tr>
+                                <!-- Frase de referencia en lo que agregamos la imagen indicada -->
+                                <td>Apple</td>
+                                <td><img src="./images/apple.jpg" alt=""></td>
+                                <td><input type="text"></td>
+                                <td>9000</td>
+                                <td><input type="checkbox"></td>
+                                <td>
+                                    <p>Actualizar</p>
+                                    <p>Borrar</p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </thead>
+                </table><!-- Fin de table -->
+               
+                <!-- Subtotal -->
+                <div class = "d-flex mb-5">
+                    <h4 class="px-3">Subtotal:<strong class="text-info">5000/-</strong></h4>
+                    
+                     <a href="index.php"><botton class = "bg-info px-3 py-2 border-0 mx-3">Continuar Comprando</botton></a>
+                     <!-- Falta agregar la página de donde se va a direccionar-->
+                     <a href="#"><botton class = "bg-secondary p-3 py-2 border-0 text-light">Checkout</botton></a>
                 </div>
-            </div> <!-- column end -->
-
-                <div class="col-md-2 bg-secondary p-0">
-                <!--side nav-->
-                <!-- marcas de guitarras que serán desplegadas-->
-                <ul class="navbar-nav me-auto text-center">
-                    <li class="nav-item bg-danger ">
-                        <a href="#" class="nav-link text-light"> <h4> Marcas Asociadas </h4> </a>
-                    </li>
-
-                    <!-- fetching the brands into the side bar from the index -->
-                    <?php
-                        getBrands();
-                    ?>
-                </ul>
-
-                <!--Categorías desplegadas -->
-
-                <ul class="navbar-nav me-auto text-center">
-
-                    <li class="nav-item bg-danger ">
-                        <a href="#" class="nav-link text-light"> <h4> Cetegorías </h4> </a>
-                    </li>
-
-                    <!-- fetching the categories into the side bar from the index -->
-
-                    <?php
-                        getCategories();
-                    ?>
-                </ul>
             </div>
-
-        </div> <!-- row end -->
-
-        <!--Last Nav Bar Child -->
-        <!-- footer -->    
-
-        <!-- including footer file -->
-        <?php include("./includes/footer.php") ?>
-          
-    </div>
+        </div>
     <!--bootstrap JS links-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
