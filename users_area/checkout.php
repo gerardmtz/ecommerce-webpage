ade816a2b41ffbbb214c00e50b8fa66fa77096bf
gerardmtz <gerartmartinez@gmail.com>
2023-04-09 22:33:09 -0600

    Se agrego el logo de la pagina

--- users_area/checkout.php
+++ users_area/checkout.php
@@ -25,7 +25,7 @@
         <!--first child-->
         <nav class="navbar navbar-expand-lg bg-danger">
         <div class="container-fluid">
-            <img src="./img/logo.png" alt="" class = "logo">
+            <img src="../img/logo.png" alt="" class = "logo">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
