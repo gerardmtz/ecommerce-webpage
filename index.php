2946447374708dda520fff9fb428bb9d88b090af
gerardmtz <gerartmartinez@gmail.com>
2023-04-11 11:21:28 -0600

    Correcciones pendientes, empezar a corregir a partir del video 57

--- index.php
+++ index.php
@@ -2,21 +2,13 @@
 
 <!-- conneting files -->
 <?php
-
-include('includes/connect.php');
-include('functions/common_function.php');
-session_start();
-
     include('includes/connect.php');
     include('functions/common_function.php');
     session_start();
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -25,36 +17,20 @@ session_start();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!--font Awesome Link-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!--
+        <style>
+            body{ overflow-x:hidden; }
+        </style>
+    -->
+    <!-- css files -->
+    <link rel="stylesheet" href="./css/style.css">
 </head>
-<!-- css files -->
-<link rel="stylesheet" href="./css/style.css">
-
-   <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Taylor</title>
-        <!--bootstrapt CSS link-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <!--font Awesome Link-->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-        <!--
-            <style>
-                body{ overflow-x:hidden; }
-            </style>
-        -->
-   </head>
-  <!-- css files -->
-  <link rel="stylesheet" href="./css/style.css">
-
-
 <body>
     <!--nav bar-->
     <div class="container-fluid p-0">
-
         <!--first child-->
         <nav class="navbar navbar-expand-lg bg-danger">
-
             <div class="container-fluid">
                 <img src="./img/logo.png" alt="" class="logo">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -70,74 +46,37 @@ session_start();
                         </li>
                         <!-- Video 59 Página de pago, logeo de cuenta en caso de no tenerla-->
                         <?php
-                        if (isset($_SESSION['username'])) {
-                            echo "<li class='nav-item'>
-                        <a class='nav-link' href='./users_area/profile.php'>Mi Cuenta</a>
-                        </li>";
-                        } else {
-                            echo "<li class='nav-item'>
-                        <a class='nav-link' href='./users_area/user_registration.php'>Registro</a>
-                        </li>";
-                        }
+                            if (isset($_SESSION['username'])) {
+                                echo "<li class='nav-item'>
+                            <a class='nav-link' href='./users_area/profile.php'>Mi Cuenta</a>
+                            </li>";
+                            } else {
+                                echo "<li class='nav-item'>
+                            <a class='nav-link' href='./users_area/user_registration.php'>Registro</a>
+                            </li>";
+                            }
                         ?>
-
-        <div class="container-fluid">
-            <img src="./img/logo.png" alt="" class = "logo">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="index.php">Página Principal</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="display_all.php">Productos</a> 
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="./users_area/user_registration.php">Registro</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Contacto</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="cart.php"><i class="fa-solid fa-cart-shopping"> <sup>  <?php cart_item(); ?> </sup> </i></a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">Monto total: $<?php total_cart_price(); ?> </a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
-                </li>
-            </ul>
-
-            <!-- search bar -->
-            <form class="d-flex" role="search" action="search_product.php" method="get">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_data">
-                <!-- <button class="btn btn-outline-light" type="submit">Buscar</button> -->
-                <input type="submit" value="Search" class="btn-outline-light" name="search_data_product">
-
-
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Contacto</a>
+                        <a class="nav-link" href="./users_area/user_registration.php">Registro</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="cart.php"><i class="fa-solid fa-cart-shopping"> <sup> <?php cart_item(); ?> </sup> </i></a>
+                        <a class="nav-link" href="#">Contacto</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Monto total: $<?php total_cart_price(); ?> </a>
+                        <a class="nav-link" href="cart.php"><i class="fa-solid fa-cart-shopping"> <sup>  <?php cart_item(); ?> </sup> </i></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
+                        <a class="nav-link" href="#">Monto total: $<?php total_cart_price(); ?> </a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
                         </li>
                     </ul>
-
                     <!-- search bar -->
                     <form class="d-flex" role="search" action="search_product.php" method="get">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_data">
+                        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" name="search_data">
                         <!-- <button class="btn btn-outline-light" type="submit">Buscar</button> -->
-                        <input type="submit" value="Search" class="btn-outline-light" name="search_data_product">
-
+                        <input type="submit" value="Buscar" class="btn-outline-light w-25" name="search_data_product">
                     </form>
                 </div>
             </div>
@@ -149,14 +88,6 @@ session_start();
         <!--second child -->
         <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
             <ul class="navbar-nav me-auto">
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Bienvenido: Invitado</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
-                </li>
-
                 <?php
                     if( !isset($_SESSION['username']) ){
                         echo "
@@ -177,7 +108,6 @@ session_start();
                         ";
                     }
                 ?>
-
             </ul>
         </nav>
 
@@ -195,11 +125,11 @@ session_start();
                     <!-- <div class="col-md-4 mb-2"> -->
                     <!-- fetching the products -->
                     <?php
-                    getProducts();
-                    get_unique_categories();
-                    get_unique_brands();
-                    // $ip = getIPAddress();  
-                    // echo 'User Real IP Address - '.$ip;
+                        getProducts();
+                        get_unique_categories();
+                        get_unique_brands();
+                        // $ip = getIPAddress();  
+                        // echo 'User Real IP Address - '.$ip;
                     ?>
                     <!-- </div> -->
                 </div>
@@ -217,14 +147,13 @@ session_start();
 
                     <!-- fetching the brands into the side bar from the index -->
                     <?php
-                    getBrands();
+                        getBrands();
                     ?>
                 </ul>
 
                 <!--Categorías desplegadas -->
 
                 <ul class="navbar-nav me-auto text-center">
-
                     <li class="nav-item bg-danger ">
                         <a href="#" class="nav-link text-light">
                             <h4> Cetegorías </h4>
@@ -232,24 +161,19 @@ session_start();
                     </li>
 
                     <!-- fetching the categories into the side bar from the index -->
-
                     <?php
-                    getCategories();
+                        getCategories();
                     ?>
                 </ul>
             </div>
-
         </div> <!-- row end -->
 
         <!--Last Nav Bar Child -->
         <!-- footer -->
-
         <!-- including footer file -->
         <?php include("./includes/footer.php") ?>
-
     </div>
     <!--bootstrap JS links-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
-
 </html>
--- users_area/profile.php
+++ users_area/profile.php
@@ -1,22 +1,18 @@
 <!-- 03/04/2023 Video 53 -->
 <!-- 04/04/2023 Video 54 -->
 
-
-
 <!-- conneting files -->
-<!--Se agregaron nuevas diagonales para el acceso de carpetas -->
 <?php 
-include('../includes/connect.php');
-include('../functions/common_function.php');
+    include('../includes/connect.php');
+    include('../functions/common_function.php');
+    session_start();
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Taylor Usuario <?php echo $_SESSION['username'] ?></title>
+    <title>Bienvenido <?php echo $_SESSION['username'] ?></title>
     <!--bootstrapt CSS link-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!--font Awesome Link-->
@@ -52,15 +48,13 @@ include('../functions/common_function.php');
 
     }
 </style>
-
 <body>
     <!--nav bar-->
     <div class="container-fluid p-0">
-
         <!--first child-->
         <nav class="navbar navbar-expand-lg bg-danger">
             <div class="container-fluid">
-                <img src="./img/logo.png" alt="" class="logo">
+                <img src="../img/logo.png" alt="" class="logo">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -74,19 +68,14 @@ include('../functions/common_function.php');
                         </li>
                         <li class="nav-item">
                             <!-- Se agrego la href del archivo profile.php -->
-                            <a class="nav-link" href="profile.php">My cuenta</a>
+                            <a class="nav-link" href="profile.php">Mi cuenta</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Contacto</a>
                         </li>
                         <li class="nav-item">
-
                             <!-- 29/03/2023 Se agrego la página cart.php y la funcion cart_item()-->
-                            <a class="nav-link" href="../cart.php"><i class="fa-solid fa-cart-shopping"></i>
-                                <sup><?php cart_item(); ?></sup> </a>
-
-                            <a class="nav-link" href="cart.php"><i class="fa-solid fa-cart-shopping"> <sup> <?php cart_item(); ?> </sup> </i></a>
-
+                            <a class="nav-link" href="../cart.php"><i class="fa-solid fa-cart-shopping"></i> <sup><?php cart_item(); ?></sup> </a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Monto total: $<?php total_cart_price(); ?> </a>
@@ -95,12 +84,12 @@ include('../functions/common_function.php');
                             <a class="nav-link disabled">Disabled</a>
                         </li>
                     </ul>
-
                     <!-- search bar -->
                     <!-- Se restructuro esta parte del código -->
-                    <form class="d-flex" action="../search_product.php" method="get">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="Search-data">
-                        <input type="submit" value="Search" class="btn btm-outline-light" name="search_data_product">
+                    <form class="d-flex" role="search" action="search_product.php" method="get">
+                        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" name="search_data">
+                        <!-- <button class="btn btn-outline-light" type="submit">Buscar</button> -->
+                        <input type="submit" value="Buscar" class="btn-outline-light w-25" name="search_data_product">
                     </form>
                 </div>
             </div>
@@ -114,33 +103,25 @@ include('../functions/common_function.php');
             <ul class="navbar-nav me-auto">
                 <!-- Se agrego una nueva función de otro video menores al 50 -->
                 <?php
-                if (!isset($_SESSION['username'])) {
-                    echo " <li class = 'nav-item'>
-                    <a class = 'nav-link' href = '#'> Bienvenido Invitado</a>
-                    </li>";
-                } else {
-                    echo "<li class = 'nav-item'>
-                    <a class = 'nav-link' href = '#'> Bienvenido" . $_SESSION['username'] . "</a>
-                    </li>";
-                }
-
-                if (!isset($_SESSION['username'])) {
-                    echo " <li class = 'nav-item'>
-                    <a class = 'nav-link' href = './users_area/user_login.php'>Login</a>
-                    <li>";
-                } else {
-                    echo "<li class = 'nav-item>
-                    <a class = 'nav-link' href = './users_area/logout.php'> Logout </a>
-                    </li>";
-                }
+                    if( !isset($_SESSION['username']) ){
+                        echo "
+                            <li class = 'nav-item'> <a class='nav-link' href='#'>Bienvenido: Invitado</a> </li>
+                        ";
+                    }else{
+                        echo "
+                            <li class = 'nav-item'> <a class='nav-link' href='#'>Bienvenido ".$_SESSION['username']." </a> </li>
+                        ";
+                    }
+                    if( !isset($_SESSION['username']) ){
+                        echo "
+                            <li class = 'nav-item'> <a class='nav-link' href='./users_area/user_login.php'>Iniciar sesión</a> </li>
+                        ";
+                    }else{
+                        echo "
+                            <li class = 'nav-item'> <a class='nav-link' href='./users_area/logout.php'>Cerrar sesión</a> </li>
+                        ";
+                    }
                 ?>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Bienvenido: Invitado</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
-                </li>
             </ul>
         </nav>
 
@@ -151,58 +132,49 @@ include('../functions/common_function.php');
         </div>
 
         <!-- fourth child -->
-
         <div class="row">
             <div class="col-md-2">
-                <ul class="navbar-nav bg-secundary text-center" style="height:100vh">
-                    <li class="nav-item bg-info">
-                        <a class="nav-link text-light" href="#">
-                            <h4>Tu Perfil</h4>
-                        </a>
-                    </li>
-                    <!-- 04/04/2023 Se agrego un username del video 54 -->
-                    <?php
-                    $username = $_SESSION['username'];
-                    $user_image = "Select * from 'user_table' where username = '$username'";
-                    $user_image = mysqli_query($con, $user_image);
-                    $row_image = mysqli_fetch_array($user_image);
-                    $user_image = $row_image['user_image'];
-                    //Falta corregir la imagen que utilizaremos
-                    echo "<li class = 'nav-item'>
-                    <img src = './user_images/$user_image' class = 'profile_img my-4' alt = ''>
-                    </li>";
-                    ?>
-
-
-                    <li class="nav-item">
-                        <!-- Falta un imagen a poner como usuario -->
-                        <img src="#" class="profile_img my-4" alt="">
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link text-light" href="profile.php">
-                            Ordenes Pendientes
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-light" href="profile.php?edit_account">
-                            Editar Cuenta
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-light" href="profile.php?my_orders">
-                            Mis Pedidos
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-light" href="profile.php?delete_account">
-                            Borrar Cuenta
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-light" href="logout.php">
-                            Desconectar
-                        </a>
+                <ul class="navbar-nav bg-secondary text-center" style="height:100vh">
+                    <li class="nav-item bg-danger">
+                        <a class="nav-link text-light" href="#"> <h4>Tu Perfil</h4> </a>
+                    
+                        <!-- 04/04/2023 Se agrego un username del video 54 -->
+                        <?php
+                            $username = $_SESSION['username'];
+                            $user_image_query = "SELECT * FROM `user_table` WHERE username = '$username'";
+                            $user_image_run = mysqli_query($con, $user_image_query);
+                            $row_image = mysqli_fetch_array($user_image_run);
+                            $user_image = $row_image['user_image'];
+                            //Falta corregir la imagen que utilizaremos
+                            echo "
+                            <li class='nav-item'> <img src='./users_images/$user_image' class='profile_img my-2' alt=''> </li>                                                  
+                            ";
+                        ?>
+                        <li class="nav-item">
+                            <a class="nav-link text-light my-2" href="profile.php">
+                                Ordenes Pendientes
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light my-2" href="profile.php?edit_account">
+                                Editar Cuenta
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light my-2" href="profile.php?my_orders">
+                                Mis Pedidos
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light my-2" href="profile.php?delete_account">
+                                Borrar Cuenta
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light my-2" href="logout.php">
+                                Cerrar sesión
+                            </a>
+                        </li>
                     </li>
                 </ul>
             </div>
@@ -227,20 +199,15 @@ include('../functions/common_function.php');
                     include( 'delete_account.php' );
 
                 }
-
                 ?>
             </div>
         </div>
-
         <!--Last Nav Bar Child -->
         <!-- footer -->
-
         <!-- including footer file -->
         <?php include("../includes/footer.php") ?>
-
     </div>
     <!--bootstrap JS links-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
-
 </html>
