2e9ab35b51d9d3d0dc567788ef713dc037294dbd
Alan <alanherrera0639@gmail.com>
2023-04-21 13:18:43 -0600

    Video 73

--- admin_area/view_products.php
+++ admin_area/view_products.php
@@ -49,7 +49,7 @@ use function PHPSTORM_META\map;
             <td><?php echo $status;?></td>
             <td><a href='index.php?edit_products=<?php echo $product_id?>' class='text-light'>
                     <i class='fa-solid fa-pen-to-square'></i></a></td>
-            <td><a href='' class='text-light'>
+            <td><a href='index.php?delete_product=<?php echo $product_id?>' class='text-light'>
                     <i class='fa-solid fa-trash'></i></a></td>
         </tr>
            <?php  
